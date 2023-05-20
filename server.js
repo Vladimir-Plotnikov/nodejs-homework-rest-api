da36a71f324be478c872b989872051f5ad2dc75c
Volodymyr <vovdata@gmail.com>
2023-05-20 11:56:43 +0200

    Hiding DB_HOST
    Hiding DB_HOST

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 const app = require('./app')
 
-const{DB_HOST}  = require("./config")
+const { DB_HOST } = process.env;
+console.log(DB_HOST);
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_HOST)
