82263f76be382dc4cbf17d964bf149dae3576c2a
Volodymyr <vovdata@gmail.com>
2023-05-20 11:28:30 +0200

    Hiding secret data
    Hiding secret data

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const app = require('./app')
 
-const DB_HOST = "mongodb+srv://Pinkcream:Pinkcream84@cluster0.fl5hhhy.mongodb.net/db-contacts?retryWrites=true&w=majority"
+const{DB_HOST}  = require("./config")
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_HOST)
